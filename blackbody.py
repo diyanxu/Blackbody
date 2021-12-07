69f428ec8f79076509e54d1c6fa3c75dd7eb302b
Rickie Li <rickie.li.x@gmail.com>
2021-12-07 16:04:55 -0500

    did all the stefan-boltzmann code stuff

--- blackbody.py
+++ blackbody.py
@@ -2,34 +2,41 @@ import numpy as np
 from scipy.optimize import curve_fit
 import matplotlib.pyplot as plt
 
+#v is volts, i is current
+def temper_fourth_func(v, i):
+    return (300+(((v/i)/1.1)-1)/(4.5*10**(-3)))**4
 
-def temper_quart_func(x, y):
-    return (300+(((x/y)/1.1)-1)/(4.5*10**(-3)))**4
+#di is current error and tq is temperature^4
+def temper_fourth_error_func(v, i, di, tq):
+    return 4*tq**(3/4)*(v*di/(i**2*1.1*4.5*10**(-3)))
 
-
+#x is temperature^4 and a is a constant
 def stefan_func(x, a):
     return a*x
 
 
 if __name__ == '__main__':
-    area_data = np.array([10.2459, 9.5128, 7.6544, 6.0169, 5.2921, 3.6674,
-                          2.3005])
-    average_area_error = np.mean([0.3982, 0.4102, 0.3852])
-    error_data = np.zeros(7)+average_area_error
-    print(error_data)
-    volt_data = np.array([10, 9, 8, 7, 6, 5, 4])
-    amp_data = np.array([0.636, 0.604, 0.561, 0.518, 0.475, 0.426, 0.374])
-    areavoltamp_data = np.array([area_data, volt_data, amp_data])
-    print(areavoltamp_data)
-    temper_quart_data = temper_quart_func(areavoltamp_data[1], 
-                                          areavoltamp_data[2])
-    print(temper_quart_data)
+    #loading data
+    stefan_data = np.loadtxt('data/stefan_dataset.txt', skiprows=1)
+    area_data = stefan_data[:,0]
+    area_error = stefan_data[:,1]
+    volt_data = stefan_data[:,2]
+    amp_data = stefan_data[:,3]
+    amp_error = stefan_data[:,4]
+    temper_fourth_data = temper_fourth_func(volt_data, amp_data)
+    temper_fourth_error = temper_fourth_error_func(volt_data, amp_data,
+                                                   amp_error,
+                                                   temper_fourth_data)
+    #curve_fit for optimized constant a to use for the model
+    popt, pcov = curve_fit(stefan_func, temper_fourth_data, area_data,
+                           sigma=area_error, absolute_sigma=True)
 
-    popt, pcov = curve_fit(stefan_func, temper_quart_data, areavoltamp_data[0],
-                           sigma=error_data, absolute_sigma=True)
-    print(popt)
-    stefan_model = stefan_func(temper_quart_data, popt)
-    plt.errorbar(temper_quart_data, areavoltamp_data[0], yerr=error_data
-                 , fmt='.')
-    plt.plot(temper_quart_data, stefan_model)
-    #amp error is approximately +- 0.01A
+    stefan_model = stefan_func(temper_fourth_data, popt)
+
+    plt.errorbar(temper_fourth_data, area_data, xerr=temper_fourth_error,
+                 yerr=area_error, fmt='.', label='Data')
+    plt.plot(temper_fourth_data, stefan_model, label='Model')
+    plt.title('Area vs Temperature^4 Plot for Stefan-Boltzmann Law ')
+    plt.xlabel('Temperature^4')
+    plt.ylabel('Area')
+    plt.legend()
